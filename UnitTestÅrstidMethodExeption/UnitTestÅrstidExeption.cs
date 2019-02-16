53f419a2dd468fb22214270033f3f207546ee4ee
felex <felicxmose@msn.com>
2019-02-16 18:21:53 +0100

    UnitTestExeption, failed to implement the test

---
+++ UnitTestÅrstidMethodExeption/UnitTestÅrstidExeption.cs
@@ -0,0 +1,23 @@
+using ClassRoom;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestÅrstidMethodExeption
+{
+    [TestClass]
+    public class UnitTestÅrstidExeption
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ////Arrange
+            //studerende student = new studerende("kim", 13, 20);
+
+            ////Act
+            //string name = student.Navn;
+
+            ////Assert
+            //Assert.ThrowsException();
+
+        }
+    }
+}
